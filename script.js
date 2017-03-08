9c049aee2eb54755330e49f2e59a5162cf663013
Eric R. Howard <ehoward@hudsonhs.com>
2017-03-08 21:46:35 +0000

    update

--- script.js
+++ script.js
@@ -6,17 +6,26 @@
 
 $(document).ready(function() {
 
-        $("button").on("click", function() {
+    $("button").on("click", function() {
 
 
-                var address = $("#address").val();
+        var zipcode = $("#zip").val();
 
-                var food = $("#food").val();
+        var food = $("#food").val();
 
-                var url = "https://api.foursquare.com/v2/venues/search?client_id=ZQS3S2ZWFOC4ZW1HQT41U2RXKRHEMEXOIPW4OZMIYFMLITZP&client_secret=IQJ30SJKZL12BHBRYI0IHQU5FAPX4BEGZHRGH40VMLFHX4GL&v=20151209&near=" +
-                    address +
-                    "&query=" + food;
+        var url = "https://api.foursquare.com/v2/venues/search?client_id=53C4ONB5UQYN2EY13QJEMANEUW5M1ZB50FKAC0ZNDMPMFRGQ&client_secret=0XFV5PM3LQUFZMJA2CKWFFHOBCPBSM3Z0PGXTEK5GEBSEA0V&v=20151209" +
+            "&near="
+        zipcode +
+            "&query=" + food;
 
+        $.get(URL, function(data) {
+            var venues = data.response.venues;
+            var myresult = "";
+            for (var i = 0; i < 2; i++) {
+                myresult += "<li>NAME: " + venues[i].name;
+                "</li>"
             }
-
-        }
+            $("#result").html(myresult);
+        })
+    });
+})
